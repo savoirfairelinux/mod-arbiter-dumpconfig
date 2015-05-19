e84600b7d7d028908ffa5999a8e3f8991ecbed0b
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-05-19 17:26:07 -0400

    setup: add unittest
    + some bits here and there.

--- setup.py
+++ setup.py
@@ -24,19 +24,36 @@
 from __future__ import with_statement
 
 import os.path as p
+import sys
 from setuptools import setup, find_packages
 
 description = "A Shinken 'module' to mirror, nearly in realtime, the Shinken ' \
               'objects attributes in a mongo db"
 
+# README
 with open('README.md') as f:
     readme = f.read()
 
+# LICENSE
 with open('LICENSE') as f:
     license = f.read()
 
+# For tests requirements:
+if sys.version_info[:2] < (3, 0):
+    unittest_pkg = "unittest2"
+else:
+    unittest_pkg = "unittest"
+
+# VERSION
 with open(p.join('mod_mongo_live_config', 'version.py')) as f:
     exec(f.read())
+try:
+    VERSION
+except NameError:
+    # so to have a default version if version.py
+    # would be empty or not declare a VERSION.
+    VERSION = "0.0.0"
+
 
 setup(
     name='mod_mongo_live_config',
@@ -50,5 +67,8 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         "pymongo==3.0.1",
+    ],
+    test_require=[
+        unittest_pkg,
     ],
 )
